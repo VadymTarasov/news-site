05517a17a0ab679474550d22e6b6b8c3cf68730f
VadymTarasov <90509364+VadymTarasov@users.noreply.github.com>
2022-01-02 20:09:41 +0200

    создал форму на странице создания категории

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
+                    <h1 class="m-0">Добавление категории</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -27,18 +27,18 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-12">
-                    <div class="col-2">
-                        <a href="{{route('admin.category.create')}}" class="btn btn-block btn-primary">Добавить</a>
-                    </div>
-                    Категории
+                    <form action="#"class="w-25">
+                        <div class="form-group">
+                            <label>название</label>
+                            <input type="text" class="form-control" placeholder="Название категории">
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Добавить">
+                    </form>
                 </div>
                 <!-- ./col -->
             </div>
             <!-- /.row -->
-
         </div><!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
 </div>
-<!-- /.content-wrapper -->
 @endsection
