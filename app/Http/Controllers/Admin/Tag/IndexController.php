00bfc175cfcbdbf99639e209c0f8dfc070066f39
VadymTarasov <90509364+VadymTarasov@users.noreply.github.com>
2022-01-05 15:16:15 +0200

    добавил CRUD для тегов

--- app/Http/Controllers/Admin/Tag/IndexController.php
+++ app/Http/Controllers/Admin/Tag/IndexController.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Category;
+namespace App\Http\Controllers\Admin\Tag;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        $categories = Category::all();
-        return view('admin.category.index',compact('categories'));
+        $tags = Tag::all();
+        return view('admin.tag.index',compact('tags'));
     }
 }
