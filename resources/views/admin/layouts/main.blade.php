4a5e716ad745c253e495b2169726cddb808ad2f1
VadymTarasov <90509364+VadymTarasov@users.noreply.github.com>
2022-01-06 17:37:38 +0200

    Добавил возможность привязывать пост к тэгам

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <title>AdminLTE 3 | Dashboard</title>
 
     <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="{{asset('plugins/select2/css/select2.min.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
@@ -13,6 +14,7 @@
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+
 
     <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
     <!-- overlayScrollbars -->
@@ -64,6 +66,7 @@
 <script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 
 <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('plugins/select2/js/select2.full.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
 
 <script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
@@ -98,6 +101,7 @@
     $(function () {
         bsCustomFileInput.init();
     });
+    $('.select2').select2()
 </script>
 <style>
     .custom-file-input:lang(en)~.custom-file-label::after{
